75f97cdb8cc8ea2079f0cdea2181b022edbea9fb
JulienDevoi <julien.devoir@request.finance>
2026-03-15 15:55:12 +0000

    update mixpanel

--- components/mixpanel-provider.tsx
+++ components/mixpanel-provider.tsx
@@ -7,7 +7,7 @@ import { createClient } from '@/lib/supabase/client'
 export default function MixpanelProvider() {
   useEffect(() => {
     mixpanel.init('a57fd33001269c679b909f70bc479996', {
-      autocapture: true,
+      autocapture: false,
       record_sessions_percent: 100,
       api_host: 'https://api-eu.mixpanel.com',
     })
